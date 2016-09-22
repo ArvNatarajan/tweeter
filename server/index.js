38137593cfe95b0e9e8e261e430c0c9d269c05c8
Arvind Natarajan <Arvind@Arvs-MBP.local>
2016-09-22 19:17:40 -0400

    adding login and cookies

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const app         = express();
+const PORT          = process.env.PORT || 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
+const cookieParser  = require('cookie-parser');
 
-const tweetsApi   = require('./api/tweets');
-const db          = require('./lib/db');
+const tweetsApi     = require('./api/tweets');
+const db            = require('./lib/db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
 
 db.connect((dbInstance) => {
   app.use('/tweets', tweetsApi(dbInstance));
